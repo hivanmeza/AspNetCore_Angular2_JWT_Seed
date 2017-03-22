09aacb6e470e1888621bebcfd8becaba4c9dd1a4
KDCLLC <info@kingdavidconsulting.com>
2017-03-22 18:19:12 -0400

    Create Initial seed project based on https://github.com/aspnet/JavaScriptServices. Run as Program not hosted in IIS.

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+﻿# AspNetCore and Angular2 with JWT Seed Project
+This project provides starter project to simply getting started with ASP.NET Core
+and SPA application like Angular.
+
+## Requirements
+* Visual Studio 2017 Community (or higher) for Windows. Any editor on Mac.
+* ASP.NET Core SDK 1.0 or higher - http://dot.net
+* Node.js 6.9 or higher (it is preferred to use NVM for NodeJs management)
+
---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace KDCLLC.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("hosting.json", optional: true)
+                        .Build();
+
+            var host = new WebHostBuilder()
+                .CaptureStartupErrors(true)
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
